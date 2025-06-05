d791b0ee6413fa4d3fc77814cb10ba9b3743a9a9
vinay <vinayreddy2126@gmail.com>
2025-06-05 07:29:42 +0530

    05/06/2025

--- Basic math and recursion/GeneratePrime.java
+++ Basic math and recursion/GeneratePrime.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class GeneratePrime {
     public static boolean isprime(int n)
     {
--- SORTING/Bubblesort.java
+++ SORTING/Bubblesort.java
@@ -4,10 +4,8 @@ public class Bubblesort {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter size of the array: ");
         int n = sc.nextInt();
-
         int arr[] = new int[n];
         System.out.println("Enter  array elements:");
         for (int i = 0; i < n; i++) {
@@ -25,11 +23,10 @@ public class Bubblesort {
                 }
             }
         }
-        System.out.println(arr);sc.close();
-        // System.out.print(" sorted array elements:");
-        // for (int i = 0; i < n; i++) {
-        //     System.out.print(arr[i]+" ");
-        // }sc.close();
+        System.out.print(" sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }sc.close();
     }
 }
         
